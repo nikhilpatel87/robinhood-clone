9e233a30ec1cb0e5784765d0313050f4e7fdd91d
Nikhil Patel <nikhilpatel87@yahoo.com>
2021-02-14 22:24:59 -0600

    Add Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import Logo from '../assets/robinhood.svg';
 import './Header.css';
+import SearchOutlined from '@material-ui/icons/SearchOutlined';
 
 function Header() {
 	return (
 		<div className="header__wrapper">
 			<div className="header__logo">
-				<img src={Logo} width={25} alt="Robinhood" />
+				<a href="/">
+					<img src={Logo} width={25} alt="Robinhood" />
+				</a>
 			</div>
 
 			<div className="header__search">
-				<div className="header__searchContainer"></div>
+				<div className="header__searchContainer">
+					<SearchOutlined />
+					<input type="text" placeholder="Search" />
+				</div>
+			</div>
+
+			<div className="header__menuItems">
+				<a href="/">Free Stocks</a>
+				<a href="/">PortFolio</a>
+				<a href="/">Cash</a>
+				<a href="/">Messages</a>
+				<a href="/">Account</a>
 			</div>
 		</div>
 	);
