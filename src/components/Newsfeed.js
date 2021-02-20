cc11d9a8490903c9631fa8c9f648600e962afbc3
Nikhil Patel <nikhilpatel87@yahoo.com>
2021-02-20 11:12:13 -0600

    Update files

--- src/components/Newsfeed.js
+++ src/components/Newsfeed.js
@@ -4,7 +4,16 @@ import './Newsfeed.css';
 function Newsfeed() {
 	return (
 		<div className="newsfeed">
-			<div className="newsfeed__container"></div>
+			<div className="newsfeed__container">
+				<div className="newsfeed__chartSection">
+					<div className="newsfeed__price">
+						<h1>$120,789</h1>
+						<small>
+							<strong>+12,345 (+10.08%)</strong> Today
+						</small>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
