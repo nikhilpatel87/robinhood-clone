a02b6c1a76511f3e9bbe56093e383350b71408af
Nikhil Patel <nikhilpatel87@yahoo.com>
2021-02-20 13:11:18 -0600

    Add initial line graph

--- src/components/LineGraph.js
+++ src/components/LineGraph.js
@@ -1,7 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Line } from 'react-chartjs-2';
+import './LineGraph.css';
 
 function LineGraph() {
-	return <div></div>;
+	const [graphData, setGraphData] = useState([]);
+	const data = [
+		{ x: 10, y: 20 },
+		{ x: 15, y: 10 },
+		{ x: 12, y: 44 },
+	];
+
+	const createMockData = () => {
+		let data = [];
+		let value = 50;
+		for (var i = 0; i < 366; i++) {
+			let date = new Date();
+			date.setHours(0, 0, 0, 0);
+			date.setDate(i);
+			value += Math.round(
+				(Math.random() < 0.5 ? 1 : 0) * Math.random() * 10
+			);
+			data.push({ x: date, y: value });
+		}
+		setGraphData(data);
+	};
+
+	useEffect(() => {
+		createMockData();
+	}, []);
+	return (
+		<div className="linegraph">
+			<Line
+				data={{
+					datasets: [
+						{
+							type: 'line',
+							data: graphData,
+							backgroundColor: 'black',
+							borderColor: 'rgba(38, 208, 43, 1)',
+							borderWidth: 2,
+							pointBorderColor: 'rgba(0,0,0,0)',
+							pointBackgroundColor: 'rgba(0,0,0,0)',
+							pointHoverBackgroundColor: 'rgba(38, 208, 43, 1)',
+							pointHoverBorderColor: '#000000',
+							pointHoverBorderWidth: 4,
+							pointHoverRadius: 6,
+						},
+					],
+				}}
+				options={{
+					maintainAspectRatio: false,
+					legend: {
+						display: false,
+					},
+					tooltips: {
+						mode: 'index',
+						intersect: false,
+					},
+					scales: {
+						xAxes: [
+							{
+								type: 'time',
+								time: {
+									format: 'MM/DD/YY',
+									tooltipFormat: 'll',
+								},
+								ticks: { display: false },
+							},
+						],
+
+						yAxes: [
+							{
+								ticks: {
+									display: false,
+								},
+							},
+						],
+					},
+				}}
+			/>
+		</div>
+	);
 }
 
 export default LineGraph;
